dd5729981f879da5a73c2cfd41d44177421a5437
Soha Jin <soha@lohu.info>
2020-07-15 09:57:05 +0800

    add example

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const { src, dest, series, parallel } = require('gulp');
+const gulpFontSpider = require('gulp-font-spider');
+const clean = require('gulp-clean');
+
+const cleanUpPreviousBuild = () => src('./dist', { read: false }).pipe(clean());
+const cleanUpOriginalFonts = () => src('./dist/**/.font-spider', { read: false }).pipe(clean());
+const copyFiles = () => src('./src/**').pipe(dest('./dist/'));
+const fontSpider = () => src('./dist/*.html')
+	.pipe(gulpFontSpider())
+	.pipe(dest('./dist/'))
+;
+
+exports.default = series(
+	cleanUpPreviousBuild,
+	copyFiles,
+	fontSpider,
+	cleanUpOriginalFonts
+)
+
+exports.clean = cleanUpPreviousBuild
